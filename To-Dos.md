5d068ec9fcac4cc2cc121f85407076d1bce45f1e
Allison <allisonkbates@gmail.com>
2021-05-01 21:32:03 -0400

    connecting to apollo

--- To-Dos.md
+++ To-Dos.md
@@ -5,6 +5,7 @@ See full to dos in Drinks V3. This list is just the list of items that will be d
 ## General
 
 - [x] to fix className does not match issue: https://github.com/vercel/next.js/issues/7322#issuecomment-811463256
+- uninstall next google fonts and find out how to load fonts
 
 ## Styles
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import Page from '../components/Page';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
+};
+
+MyApp.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
 }
+
+export default withData(MyApp);
